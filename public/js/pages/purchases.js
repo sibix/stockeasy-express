98071f4ea038653d0cf35319a86f3760dd2b63bd
sibix <sibix777@gmail.com>
2026-03-21 20:35:08 +0530

    set purchase history as default view

--- public/js/pages/purchases.js
+++ public/js/pages/purchases.js
@@ -29,7 +29,7 @@ document.addEventListener('DOMContentLoaded', async function () {
   await Promise.all([loadCategories(), loadSettings()]);
   populateApplyAllCatSelect();
   bindGlobalEvents();
-  switchTab('new');
+  switchTab('history');
 });
 
 function todayISO() {
